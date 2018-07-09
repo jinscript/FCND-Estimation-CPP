b4e4f6e4d77395ed1e7621a3e1ccd0b0b5122bed
Jin Sun <jins@uber.com>
2018-07-09 00:41:18 -0700

    partial

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -322,8 +322,9 @@ void QuadEstimatorEKF::UpdateFromMag(float magYaw)
   //  - The magnetomer measurement covariance is available in member variable R_Mag
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
+  hPrime(0, 6) = 1;
   zFromX(0) = ekfState(6);
-  float err = z(0) - zFromX(0);
+  float err = magYaw - ekfState(6);
 
   if (err > F_PI)
     z(0) -= 2.f * F_PI;
